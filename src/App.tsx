50e9b6050ab1967f6e515b10b1254a7f3afdf1d9
Leda <ledathemis@protonmail.com>
2022-08-16 16:39:41 +0000

    feat: setup routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Post from './pages/Post';
+import Register from './pages/Register';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="posts">
+              <Route path=":postId" element={<Post />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
